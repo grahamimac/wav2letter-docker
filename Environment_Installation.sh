00d99936298f368e91e49d9574bc24408b222ab5
grahamimac <graham3333@yahoo.com>
2018-05-02 21:13:43 -0400

    Add MKL installation and test

--- Environment_Installation.sh
+++ Environment_Installation.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
-# Install wav2letter CPU
+# Install wav2letter CPU - modified from https://github.com/facebookresearch/wav2letter
 
 # Install dependencies
 apt-get update -y
-apt-get install git wget curl cmake build-essential apt-utils unzip -y
+apt-get install git wget curl cmake build-essential apt-utils unzip apt-transport-https -y
 apt-get install libboost-dev libboost-system-dev libboost-thread-dev libboost-test-dev libboost-all-dev zlib1g-dev bzip2 libbz2-dev liblzma-dev -y
 apt-get install libfftw3-dev libfftw3-doc libsndfile-dev -y
+
+# Install MKL - modified from https://github.com/eddelbuettel/mkl4deb/blob/master/script.sh
+cd /tmp
+wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+sh -c 'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'
+apt-get update -y
+apt-get install intel-mkl-64bit-2018.2-046 -y
+update-alternatives --install /usr/lib/x86_64-linux-gnu/libblas.so     libblas.so-x86_64-linux-gnu      /opt/intel/mkl/lib/intel64/libmkl_rt.so 50
+update-alternatives --install /usr/lib/x86_64-linux-gnu/libblas.so.3   libblas.so.3-x86_64-linux-gnu    /opt/intel/mkl/lib/intel64/libmkl_rt.so 50
+update-alternatives --install /usr/lib/x86_64-linux-gnu/liblapack.so   liblapack.so-x86_64-linux-gnu    /opt/intel/mkl/lib/intel64/libmkl_rt.so 50
+update-alternatives --install /usr/lib/x86_64-linux-gnu/liblapack.so.3 liblapack.so.3-x86_64-linux-gnu  /opt/intel/mkl/lib/intel64/libmkl_rt.so 50
+echo "/opt/intel/lib/intel64"     >  /etc/ld.so.conf.d/mkl.conf
+echo "/opt/intel/mkl/lib/intel64" >> /etc/ld.so.conf.d/mkl.conf
+ldconfig
+echo "MKL_THREADING_LAYER=GNU" >> /etc/environment
+
+# Update bashrc file to include mkl
+echo 'export PATH=$HOME/usr/bin:$PATH' >> ~/.bashrc
+echo 'INTEL_DIR=/opt/intel/lib/intel64' >> ~/.bashrc
+echo 'MKL_DIR=/opt/intel/mkl/lib/intel64' >> ~/.bashrc
+echo 'MKL_INC_DIR=/opt/intel/mkl/include' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$INTEL_DIR:$MKL_DIR' >> ~/.bashrc
+echo 'export CMAKE_LIBRARY_PATH=$LD_LIBRARY_PATH' >> ~/.bashrc
+echo 'export CMAKE_INCLUDE_PATH=$CMAKE_INCLUDE_PATH:$MKL_INC_DIR' >> ~/.bashrc
+source ~/.bashrc
 
 # LuaJIT and LuaRocks
 git clone https://github.com/torch/luajit-rocks.git
